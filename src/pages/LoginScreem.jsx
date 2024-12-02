e83d8391dbd58b520590e35b3a9e292b97e0930a
LucasFerruchi <lucasferruchi05@gmail.com>
2024-12-02 20:40:41 -0300

    estructura de formulario LogIn

--- src/pages/LoginScreem.jsx
+++ src/pages/LoginScreem.jsx
@@ -5,8 +5,37 @@ function LoginScreem() {
     <>
       <div className="container">
         <div className="row">
-          <div className="col">
-            <h2>Login Screen</h2>
+          <div className="col text-center">
+            <h3>
+              Sing in to <i className="fa fa-btc" aria-hidden="true"></i>
+              comReact1 Coins
+            </h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col col-md-6 offset-md-3">
+            <form>
+              <div className="mb-3 d-grid">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  //required
+                />
+              </div>
+              <div className="mb-3 d-grid">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                />
+              </div>
+              <div className="mb-3 d-grid">
+                <button className="btn btn-success">Sign in</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
