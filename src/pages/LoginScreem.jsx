15e34f5c19c3b1ea4e77a08bd48eb136165226c3
LucasFerruchi <lucasferruchi05@gmail.com>
2024-12-04 20:29:23 -0300

    control del submit del formulario LogIn y vaidacion de datos

--- src/pages/LoginScreem.jsx
+++ src/pages/LoginScreem.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LoginScreem() {
+  const navigate = useNavigate();
+
   //? Crear UN estado POR CAMPO
   //   const [email, setEmail] = useState("");
   // //   console.log(email);
@@ -19,8 +22,35 @@ function LoginScreem() {
     setFormValues({
       //! SPREAD OPERATOR "...": crea EL NUEVO OBJETO
       ...formValues,
+
+      //! Identificar CADA UNO  de los campos del form
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //USUARIO REGISTRADO DE EJEMPLO - DB
+    const user = {
+      email: "lucas@lucas.com",
+      password: "123456",
+    };
+
+    //? validar que se completen los campos
+    if (!formValues.email || !formValues.password) {
+      alert("Debe completar los campos obligatorios!");
+    }
+
+    if (
+      formValues.email === user.email &&
+      formValues.password === user.password
+    ) {
+      // alert("Datos correctos");
+      navigate("/");
+    } else {
+      alert("Email o password incorrecto!");
+    }
   };
 
   return (
@@ -36,14 +66,14 @@ function LoginScreem() {
         </div>
         <div className="row">
           <div className="col col-md-6 offset-md-3">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="mb-3 d-grid">
                 <label>Email</label>
                 <input
                   type="email"
                   className="form-control"
                   name="email"
-                  //   required
+                  // required
                   //   onChange={(e) => {
                   //     setEmail(e.target.value);
                   //   }}
